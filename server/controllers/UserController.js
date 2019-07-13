89565571986271a01563e0f5203602e264f9189d
ChidiChuks <chidiebere_chukwuma@yahoo.com>
2019-07-13 16:07:29 +0100

    feature(sign-in): user can sign in [Finishes #167191201]

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -87,6 +87,7 @@ const User = {
         }
     },
 
+
     async signIn(req, res) {
         delete req.headers['x-auth'];
         if (validateData(['email', 'password'], req.body) || !validEmail(req.body.email)) {

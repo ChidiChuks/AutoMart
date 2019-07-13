11eaf454f54ad4336f9bf6c2dcc34e8b961f2a4b
ChidiChuks <chidiebere_chukwuma@yahoo.com>
2019-07-13 17:41:53 +0100

    feature(user-view-ad): user can view all unsold ads [Finishes #167191282]

--- server/test/integration/UsersController.spec.js
+++ server/test/integration/UsersController.spec.js
@@ -134,6 +134,7 @@ describe('User', () => {
         });
     });
 
+
     // user sign in
     describe('User Signin', () => {
         // it('should login a user and set token in the header', async() => {
